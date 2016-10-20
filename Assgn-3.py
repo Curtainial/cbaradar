fc6ed7f6927e8917edb0d7596c5a4e834c3a0a40
Cyrus Azimi <cbaradar@ucsc.edu>
2016-10-20 01:59:55 -0700

    FAILS

--- Assgn-3.py
+++ Assgn-3.py
@@ -2,9 +2,9 @@ import random,sys
 
 
 start = float((input("What is the starting number (1-499)? " )))
-while start < 1:
+while start < 1 and start > 499:
     start = float((input("What is the starting number (1-499)? " )))
-
+#these lines prevent the user from inputting anything other than the options provided
 start_min = int(start-1)
 start_max = int(start+1)
 
@@ -12,69 +12,69 @@ start_max = int(start+1)
 max = (input("What is the maximum value (" + str(start_max) + "-500)" + "? "))
 while int(max) <= int(start) or int(max) > 500:
     max = (input("What is the maximum value (" + str(start_max) + "-500)" + "? "))
+#these lines prevent the user from inputting anything other than the options provided
 
 min = (input("What is the minimum number (0-" + str(start_min) + ")?" ))
 while int(min) >= int(start) or int(min) < 0:
     min = (input("What is the minimum number (0-" + str(start_min) + ")?" ))
+#these lines prevent the user from inputting anything other than the options provided
 
 
 right_percent = int(input("What's the percentage chance of moving right (1-99)? "))
 while int(right_percent) <= 0 or int(right_percent) >= 100:
     right_percent = int(input("What's the percentage chance of moving right (1-99)? "))
+#these lines prevent the user from inputting anything other than the options provided
 
-walks = int(input("How many walks (1-1000)? "))
-while int(walks) <= 0 or int(walks) >= 1001:
+
+walks = int(input("How many walks (1-10000)? "))
+while int(walks) <= 0 or int(walks) >= 10001:
     walks = int(input("How many walks (1-1000)? "))
+#these lines prevent the user from inputting anything other than the options provided
+steps = 0
+place = start + steps
 
-answer = True
 
+#allows the following loops to run
 
 def randomwalk():
     steps = 0
+    place = start + steps
 
+    fullwalks = 0
 
-    while answer == True:
-        place = 0
-        while steps < 5000:
+    while steps < 5000:
+        rightwalks = 0
+        leftwalks = 0
+        right_prob = random.randint(1,100)
 
-            steps = steps + 1
+        if right_prob >= right_percent:
+            place = steps - 1 + start
+        elif right_prob <=  right_percent:
 
+            place = steps + 1 + start
+            return place
+        elif steps == 5000:
+            fullwalks +=1
+            return fullwalks
 
-            if random.randint(1,100) >= right_percent:
+        elif place == min:
 
-                place = steps + 1
-                return steps
+            leftwalks +=1
+            return leftwalks
+        elif place == max:
 
-            elif():
+            rightwalks +=1
+            return rightwalks
 
-                place = steps - 1
-                return steps
-
-    if steps == 5000:
-        fullwalks = 0
-        answer == False
-        return steps
-
-    elif place == min:
-        minwalks = 0
-        answer == False
-        return steps
-
-    elif place == max:
-        maxwalks = 0
-        answer == False
-        return steps
-
+    steps += 1
+    return steps
 
 
 
 
 while walks != 0:
     print(randomwalk())
-    walks = walks - 1
-
-print("Number of walks ")
-
+    walks -=1
 
 
 
