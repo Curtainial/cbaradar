56c07bc6bdb7dee53f58955c0990e001801ed2e5
Cyrus Azimi <cbaradar@ucsc.edu>
2016-10-26 04:04:40 -0700

    Wrote my plan. Due today worried.

--- asgn4.py
+++ asgn4.py
@@ -1,18 +1,15 @@
 import string
 alpha = "ABCDEFGHIJKLHMNOPQRSTUVWXYZ"
-
+#still idk what to do with this
 def encrypt(message,key):
     print(message)
     while len(key) < len(message):
         key += key
+#Gets the key lined up with the message.
+    print(key[:len(message)])
 
-    return key[:len(message)]
-
-
-
-
-
-
+#assign values to letters, then use those values to add for encryption
+#subtract for decryption and use modulo 26 if greater than 26
 
 
 
